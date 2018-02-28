377c674b139408591fa86e17646df44ba585402f
C-002 <resurgent0213@live.cn>
2018-02-28 23:51:02 +0000

    Preview

--- Preview/make_db_file.py
+++ Preview/make_db_file.py
@@ -17,7 +17,23 @@ def storeDbase(db, dbfilename = dbfilename):
     print (ENDDB, file = dbfile)
     dbfile.close()
 
-
+def loadDbase(dbfilename = dbfilename):
+    "parse data to reconsyruct database"
+    dbfile = open(dbfilename)
+    import sys
+    sys.stdin = dbfile
+    db = {}
+    key = input()
+    while key != ENDDB:
+        rec = {}
+        field = input()
+        while field != ENDREC:
+            name, value = field.split(RECSEP)
+            rec[name] = eval(value)
+            field = input()
+        db[key] = rec
+        key = input()
+    return db       
 
 if __name__ == '__main__':
     from initdata import db
